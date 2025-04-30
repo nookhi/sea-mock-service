79deaf52da456f7a27fe9117a3402c5428723a8a
nookhi <62215265+nookhi@users.noreply.github.com>
2025-04-30 18:36:29 +0300

    Add files via upload

---
+++ sea-mock-service/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ServiceModel;
+
+class Program
+{
+    static void Main()
+    {
+        Uri baseAddress = new Uri("http://localhost:80/DocumentService");
+
+        using (ServiceHost host = new ServiceHost(typeof(DocumentService), baseAddress))
+        {
+            var binding = new BasicHttpBinding();
+            host.AddServiceEndpoint(typeof(IDocumentServicePort), binding, "");
+
+            // Включаем метаданные для WSDL
+            var smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
+
+            host.Open();
+
+            Console.WriteLine("SOAP Mock Service is running...");
+            Console.WriteLine("Access WSDL at: " + baseAddress + "?wsdl");
+            Console.WriteLine("Press ENTER to stop.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sea-mock-service/SoapService.cs
@@ -0,0 +1,53 @@
+using System;
+using System.ServiceModel;
+
+[ServiceContract(Namespace = "http://ws.ustorage.idmt.ru")]
+public interface IDocumentServicePort
+{
+    [OperationContract]
+    getDocumentResponse getDocument(getDocument request);
+
+    [OperationContract]
+    getVersionsResponse getVersions(getVersions request);
+    
+    // Добавь остальные методы по аналогии при необходимости
+}
+
+[DataContract(Namespace = "http://ws.ustorage.idmt.ru")]
+public class getDocument
+{
+    [DataMember] public string documentId { get; set; }
+}
+
+[DataContract(Namespace = "http://ws.ustorage.idmt.ru")]
+public class getDocumentResponse
+{
+    [DataMember] public string result { get; set; } = "Mocked Document Content";
+}
+
+[DataContract(Namespace = "http://rt.fs.documentum.emc.com/")]
+public class getVersions
+{
+    [DataMember] public string documentId { get; set; }
+}
+
+[DataContract(Namespace = "http://rt.fs.documentum.emc.com/")]
+public class getVersionsResponse
+{
+    [DataMember] public string[] versions { get; set; } = new[] { "v1", "v2" };
+}
+
+public class DocumentService : IDocumentServicePort
+{
+    public getDocumentResponse getDocument(getDocument request)
+    {
+        Console.WriteLine($"getDocument called with ID: {request.documentId}");
+        return new getDocumentResponse();
+    }
+
+    public getVersionsResponse getVersions(getVersions request)
+    {
+        Console.WriteLine($"getVersions called for document: {request.documentId}");
+        return new getVersionsResponse();
+    }
+}
